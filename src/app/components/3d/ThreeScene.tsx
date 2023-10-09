43f880c42e38bd749bc4c8a903752c3fd16a84f1
ponta10 <mponta2002@gmail.com>
2023-10-09 21:46:36 +0900

    畑完成

--- src/app/components/3d/ThreeScene.tsx
+++ src/app/components/3d/ThreeScene.tsx
@@ -1,45 +1,46 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Sky, useGLTF, useTexture } from '@react-three/drei';
-import { useMemo } from 'react';
-
-function Cactus({ position, scale }: any) {
-    const gltf = useGLTF('/air_plant_v1.0.glb');
-    const scene = useMemo(() => gltf.scene.clone(), [gltf]);
-    return <primitive object={scene} position={position} scale={scale} />;
-}
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sky, useTexture } from "@react-three/drei";
+import { Cactus, Corn, Flower, Plant, Tomato } from "./Plant";
 
 function Desert() {
-    const sandTexture = useTexture('/desert2.jpeg');
-    const numCacti = 100;
-    const positions = Array.from({ length: numCacti }, () => ([
-        (Math.random() - 0.5) * 20,
-        -0.8,                      
-        (Math.random() - 0.5) * 20
-    ]));
-    return (
-        <>
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[0, 10, 5]} intensity={1} color={'#ffffff'} />
-            {positions.map((position, index) => (
-                <Cactus key={index} position={position} scale={[6, 6, 6]} />
-            ))}
-            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-                <planeGeometry args={[20, 20, 100, 100]} />
-                <meshStandardMaterial map={sandTexture} />
-            </mesh>
-        </>
-    );
+  const sandTexture = useTexture("/desert2.jpeg");
+  const numCacti = 100;
+  const positions = Array.from({ length: numCacti }, () => [
+    (Math.random() - 0.5) * 20,
+    -0.8,
+    (Math.random() - 0.5) * 20,
+  ]);
+  return (
+    <>
+      <ambientLight intensity={2} />
+      <directionalLight position={[0, 5, 5]} intensity={3} color={"#FDB86D"} />
+      {positions.map((position, index) => (
+        <Cactus key={index} position={position} scale={[6, 6, 6]} />
+      ))}
+      <Plant key={101} position={[0, -1, 1]} scale={[0.005, 0.005, 0.005]} />
+      <Flower key={102} position={[1, -1, 1]} scale={[0.1, 0.1, 0.1]} />
+      <Tomato key={103} position={[2, -1, 1]} scale={[1, 1, 1]} />
+      <Corn key={104} position={[-2, -1, 1]} scale={[1, 1, 1]} />
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+        <planeGeometry args={[20, 20, 100, 100]} />
+        <meshStandardMaterial map={sandTexture} />
+      </mesh>
+    </>
+  );
 }
-
-
 
 export default function DesertScene() {
   return (
-    <Canvas style={{ width: '100%', height: '100vh' }}>
-      {/* <OrbitControls /> */}
-      <Sky sunPosition={[0, 1, 0]} />
+    <Canvas style={{ width: "100%", height: "100vh" }}>
+      <OrbitControls />
+      <Sky
+        sunPosition={[0, 0.08, -0.15]}
+        turbidity={5}
+        rayleigh={0.1}
+        mieCoefficient={0.0005}
+        mieDirectionalG={0.9}
+      />
       <Desert />
     </Canvas>
   );
 }
-
