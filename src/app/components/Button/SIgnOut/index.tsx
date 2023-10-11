36c71980e88ab67ddeb33bdfae30f6cc2e5a788b
ponta10 <mponta2002@gmail.com>
2023-10-11 15:55:13 +0900

    Buttonカスタム

---
+++ src/app/components/Button/SIgnOut/index.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { signOut } from "next-auth/react";
+import { Button } from "..";
+
+export default function SignOutButton() {
+  return (
+    <Button text="SignOut" bgcolor="bg-base" fontSize="lg" onClick={() => signOut()} />
+  );
+}
---
+++ src/app/components/Button/SignIn/index.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { signIn } from "next-auth/react";
+import { Button } from "..";
+
+export default function SignInButton() {
+  return (
+    <Button text="SignIn" bgcolor="bg-base" fontSize="lg" onClick={() => signIn()} />
+  );
+}
