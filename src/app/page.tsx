cdb2ad68b5e898f190af8569b3e756aeb7c2f1cf
ponta10 <mponta2002@gmail.com>
2023-10-13 15:55:57 +0900

    ページ構成変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { getGithubData } from "@/function/getGithubData";
 import { getServerSession } from "next-auth/next";
 import { nextAuthOptions } from "@/libs/next-auth/options";
-import { Top } from "../components/Top";
 import SignInButton from "../components/Button/SignIn";
 import Image from "next/image";
-import logo from "../../public/logoWhite.png";
+import logoWhite from "../../public/logoWhite.png";
+import logo from "../../public/logo.png";
 import field from "../../public/field.jpg";
-import { getGithubDetailData } from "@/function/getGithubDetailData";
+import WatchDataButton from "@/components/Button/WatchData";
+import SignOutButton from "@/components/Button/SIgnOut";
+import { DesertScene } from "@/components/3d/ThreeScene";
 
 export default async function Home() {
   const session = await getServerSession(nextAuthOptions);
@@ -22,7 +24,7 @@ export default async function Home() {
         className="w-screen h-screen relative"
       >
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 flex flex-col gap-8">
-          <Image width={600} height={80} alt="logo" src={logo} priority />
+          <Image width={600} height={80} alt="logo" src={logoWhite} priority />
           <SignInButton />
           <p className="text-white text-xl">
             〜あなたのGitHubに応じて畑が育つ〜
@@ -38,5 +40,16 @@ export default async function Home() {
     "2023-08-31T00:00:00"
   );
 
-  return <Top />;
+  return (
+    <>
+    <header className="fixed top-0 left-0 z-10 w-screen h-24 bg-white shadow-md flex items-center justify-between px-10">
+      <Image width={180} height={80} alt="logo" src={logo} priority style={{ width: "auto" }} />
+      <div className="flex gap-4">
+        <WatchDataButton />
+        <SignOutButton />
+      </div>
+    </header>
+    <DesertScene data={githubData.total} />
+  </>
+  );
 }
