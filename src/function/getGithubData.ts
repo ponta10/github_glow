9936ede5753c804200b0f63ab966e5d46c78cc09
ponta10 <mponta2002@gmail.com>
2023-10-13 15:21:24 +0900

    関数処理切り分け

--- src/function/getGithubData.ts
+++ src/function/getGithubData.ts
@@ -10,31 +10,8 @@ export async function getGithubData(
                 name
                 email
                 avatarUrl
-                pullRequests { totalCount }
-                issues { totalCount }
-                issueComments { totalCount }
                 contributionsCollection(from: $from, to: $to) {
-                    totalRepositoryContributions
                     contributionCalendar {totalContributions}
-                    totalCommitContributions
-                    totalPullRequestContributions
-                    totalIssueContributions
-                    totalPullRequestReviewContributions
-                    repositoryContributions(first: 100) {
-                        nodes {
-                            repository {
-                                name
-                                languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
-                                    edges {
-                                        size
-                                        node {
-                                            name
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
                 }
             }
         }
@@ -63,27 +40,6 @@ export async function getGithubData(
     const data = await response.json();
     const user = data.data?.viewer;
 
-    const repoContributions = data.data.viewer.contributionsCollection.repositoryContributions.nodes;
-
-    const languageSizes: { [key: string]: number } = {};
-    repoContributions.forEach((contribution: any) => {
-      contribution && contribution.repository.languages.edges.forEach((langEdge: any) => {
-        const langName = langEdge.node.name;
-        languageSizes[langName] = (languageSizes[langName] || 0) + langEdge.size;
-      });
-    });
-
-    const totalSize = Object.values(languageSizes).reduce((sum, size) => sum + size, 0);
-
-    const languagePercent = Object.keys(languageSizes).map((langName) => {
-        const size = languageSizes[langName];
-        const percent = ((size / totalSize) * 100).toFixed(2);
-        return {
-            name: langName,
-            percent: parseFloat(percent)
-        };
-    });
-
     return {
       user: {
         login: user.login,
@@ -91,13 +47,7 @@ export async function getGithubData(
         email: user.email,
         avatarUrl: user.avatarUrl,
       },
-      prCount: user.contributionsCollection.totalPullRequestContributions,
-      issueCount: user.contributionsCollection.totalIssueContributions,
-      reviewCount: user.contributionsCollection.totalPullRequestReviewContributions,
-      commitCount: user.contributionsCollection.totalCommitContributions,
-      repoLanguageData: languagePercent,
       total: user.contributionsCollection.contributionCalendar.totalContributions,
-      commitRepo: user.contributionsCollection.totalRepositoryContributions,
     };
   } catch (error) {
     console.error("Error fetching data: ", error);
