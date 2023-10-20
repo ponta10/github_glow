d508c9b489544c904d685d2dcf96eb780be326e5
miura kota <94046278+ponta10@users.noreply.github.com>
2023-10-20 16:35:40 +0900

    データ取得関数切り分け (#2)

---
+++ src/utils/types.ts
@@ -0,0 +1,29 @@
+export interface ContributionDay {
+  date: string;
+  contributionCount: number;
+}
+
+export interface Week {
+  contributionDays: ContributionDay[];
+}
+
+export interface LanguageNode {
+  name: string;
+}
+
+export interface LanguageEdge {
+  size: number;
+  node: LanguageNode;
+}
+
+export interface Languages {
+  edges: LanguageEdge[];
+}
+
+export interface Repository {
+  languages: Languages;
+}
+
+export interface RepositoryContribution {
+  repository: Repository;
+}
