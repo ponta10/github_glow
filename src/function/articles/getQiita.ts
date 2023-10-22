1b7c96772bc5389e47eaf68c8aed995b48a07966
ponta10 <mponta2002@gmail.com>
2023-10-22 17:42:50 +0900

    stackoverflow質問取得

--- src/function/articles/getQiita.ts
+++ src/function/articles/getQiita.ts
@@ -6,6 +6,14 @@ interface Article {
     pubDate: string;
 };
 
+function formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}/${month}/${day}`;
+}
+
 export async function fetchArticles(): Promise<Article[]> {
     try {
         const response = await fetch('https://qiita.com/popular-items/feed');
@@ -30,7 +38,7 @@ export async function fetchArticles(): Promise<Article[]> {
         const quiitaArticles: Article[] = rawArticles.map((rawArticle: any) => ({
             title: rawArticle.title[0],
             link: rawArticle.link[0].$.href,
-            pubDate: rawArticle.published[0],
+            pubDate: formatDate(rawArticle.published[0]),
         }));
 
         return quiitaArticles;
