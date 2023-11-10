cde3574842d5c4d8555addfa425cc8dc39bdd909
ponta10 <mponta2002@gmail.com>
2023-11-10 15:29:45 +0900

    lint修正

--- src/app/watch/BarGraph/BarGraph.test.tsx
+++ src/app/watch/BarGraph/BarGraph.test.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import BarGraph from '.';
-import { GraphData } from '@/utils/const';
+import BarGraph from ".";
+import { GraphData } from "@/utils/const";
 
-describe('BarGraph', () => {
+describe("BarGraph", () => {
   const mockData: GraphData[] = [
-    { name: 'Page A', value: 400 },
-    { name: 'Page B', value: 300 },
-    { name: 'Page C', value: 300 },
-    { name: 'Page D', value: 200 },
+    { name: "Page A", value: 400 },
+    { name: "Page B", value: 300 },
+    { name: "Page C", value: 300 },
+    { name: "Page D", value: 200 },
   ];
 
-  it('クラッシュせずにレンダリングされること', () => {
+  it("クラッシュせずにレンダリングされること", () => {
     const { container } = render(<BarGraph data={mockData} />);
 
-    expect(container.querySelector('svg')).toBeInTheDocument();
+    expect(container.querySelector("svg")).toBeInTheDocument();
   });
 
-  it('各データエントリに対して正しいバーがレンダリングされること', () => {
+  it("各データエントリに対して正しいバーがレンダリングされること", () => {
     render(<BarGraph data={mockData} />);
-    mockData.forEach(dataEntry => {
+    mockData.forEach((dataEntry) => {
       expect(screen.getByText(dataEntry.name)).toBeInTheDocument();
-    //   expect(screen.getByText(dataEntry.value.toString())).toBeInTheDocument();
+      //   expect(screen.getByText(dataEntry.value.toString())).toBeInTheDocument();
     });
   });
 
-  it('データが空の場合でもクラッシュせずにレンダリングされること', () => {
+  it("データが空の場合でもクラッシュせずにレンダリングされること", () => {
     const { container } = render(<BarGraph data={[]} />);
-    expect(container.querySelector('svg')).toBeInTheDocument();
+    expect(container.querySelector("svg")).toBeInTheDocument();
   });
 
-  it('不正なデータが与えられた場合にエラーメッセージが表示されること', () => {
-    const badData = [{ name: 'Page X', value: null }];
+  it("不正なデータが与えられた場合にエラーメッセージが表示されること", () => {
+    const badData = [{ name: "Page X", value: null }];
     render(<BarGraph data={badData as unknown as GraphData[]} />);
   });
 });
